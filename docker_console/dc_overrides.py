c9d6ee9ecba3f62e46eece772aa8a7bdb43526a0
Damian Sikora <damian.sikora@droptica.pl>
2017-02-07 13:17:51 +0100

    cleanup

--- docker_console/dc_overrides.py
+++ docker_console/dc_overrides.py
@@ -20,14 +20,21 @@
 #     print self.config.DRUPAL[self.config.drupal_site]['ADMIN_USER']
 #
 # Drush.uli = drush_uli_local
+#
+# # replace/add new commands
+# commands_overrides = {
+#     'localtest': [
+#         'confirm_action',
+#         'drush.localtest("upwd %s --password=123" % self.config.DRUPAL[self.config.drupal_site]["ADMIN_USER"])'
+#     ],
+#     'drush_uli': [
+#         'confirm_action("no")',
+#         'drush.uli'
+#     ],
+# }
 
 
-# replace/add new commands
 commands_overrides = {
-    'drush_uli': [
-        'confirm_action("no")',
-        'drush.uli'
-    ],
     'build-in-docker': [
         'archive.unpack_files(True)',
         'drupal_settings.copy_settings("drupal7", True)',
