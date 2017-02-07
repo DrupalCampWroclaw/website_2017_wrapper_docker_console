43c8dea30b9e8e78bd3e805d7f2e7f3ea9f33db5
Damian Sikora <damian.sikora@droptica.pl>
2017-02-07 12:56:06 +0100

    cleanup

--- docker_console/dc_overrides.py
+++ docker_console/dc_overrides.py
@@ -1,40 +1,29 @@
 # import classes to override
-from docker_console.web.engines.drupal7.drush import Drush
-from docker_console.web.engines.drupal7.builder import Builder
-
-# add new methods
-class DrushLocal:
-    def localtest(self, text):
-        print text
-
-Drush.__bases__ += (DrushLocal,)
-
-class BuilderLocal:
-    def printlocal(self):
-        self.drush.localtest('printlocal')
-
-Builder.__bases__ += (BuilderLocal,)
-
-# override existing method
-def drush_uli_local(self):
-    print self.config.DRUPAL[self.config.drupal_site]['ADMIN_USER']
-
-Drush.uli = drush_uli_local
+# from docker_console.web.engines.drupal7.drush import Drush
+# from docker_console.web.engines.drupal7.builder import Builder
+#
+# # add new methods
+# class DrushLocal:
+#     def localtest(self, text):
+#         print text
+#
+# Drush.__bases__ += (DrushLocal,)
+#
+# class BuilderLocal:
+#     def printlocal(self):
+#         self.drush.localtest('printlocal')
+#
+# Builder.__bases__ += (BuilderLocal,)
+#
+# # override existing method
+# def drush_uli_local(self):
+#     print self.config.DRUPAL[self.config.drupal_site]['ADMIN_USER']
+#
+# Drush.uli = drush_uli_local
 
 
 # replace/add new commands
 commands_overrides = {
-    'build': [
-        'confirm_action',
-        'docker.docker_run("docker-console build-in-docker")',
-        'docker.chown',
-        'docker.setfacl',
-        'chmod_files'
-    ],
-    'localtest': [
-        'confirm_action',
-        'drush.localtest("upwd %s --password=123" % self.config.DRUPAL[self.config.drupal_site]["ADMIN_USER"])'
-    ],
     'drush_uli': [
         'confirm_action("no")',
         'drush.uli'
@@ -48,7 +37,7 @@ commands_overrides = {
 
         'drush.updb',
 
-#        'drush.cc',
+        'drush.cc_all',
         'drush.change_password',
         'drush.uli',
     ]
